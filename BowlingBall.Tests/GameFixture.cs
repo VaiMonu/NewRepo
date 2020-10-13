078839a0d053c90c8f4456f163ec6d8d86933732
VaiMonu <landevaibhav02@gmail.com>
2020-10-13 12:38:15 +0530

    test

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        private Game _game;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void TestRollingAllGutterBalls()
+        {
+            for (int i = 0; i < 10; i++) { _game.Roll(0, 0); }
+            Assert.AreEqual(0, _game.Score());
+        }
+
+        [TestMethod]
+        public void TestRollingAllOnes()
+        {
+            for (int i = 0; i < 10; i++) { _game.Roll(1, 1); }
+            Assert.AreEqual(20, _game.Score());
+        }
+
+        [TestMethod]
+        public void TestFirstFrameSpareOthersRollingAllTwos()
+        {
+            _game.Roll(9, 1);
+            for (int i = 0; i < 9; i++) { _game.Roll(2, 2); }
+            Assert.AreEqual(48, _game.Score());
+        }
+
+        [TestMethod]
+        public void TestFirstTwoFramesSparesOthersAllRollTwos()
+        {
+            _game.Roll(5, 5);
+            _game.Roll(5, 5);
+            for (int i = 0; i < 8; i++) { _game.Roll(2, 2); }
+            Assert.AreEqual(59, _game.Score());
+        }
+
+        [TestMethod]
+        public void TestGameOver()
+        {
+            for (int i = 0; i < 10; i++) { _game.Roll(0, 0); }
+            _game.Roll(0, 0);
+        }
+
+        [TestMethod]
+        public void TestFirstFrameStrikeOthersRollAllTwos()
+        {
+            _game.RollStrike();
+            for (int i = 0; i < 9; i++) { _game.Roll(2, 2); }
+            Assert.AreEqual(50, _game.Score());
+        }
+
+        [TestMethod]
+        public void TestFirstTwosFramesStrikeOthersRollAllTwos()
+        {
+            _game.RollStrike();
+            _game.RollStrike();
+            for (int i = 0; i < 8; i++) { _game.Roll(2, 2); }
+            Assert.AreEqual(68, _game.Score());
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            for (int i = 0; i < 9; i++) { _game.RollStrike(); }
+            _game.RollLastFrame(10, 10, 10);
+            Assert.AreEqual(300, _game.Score());
+        }
+
+        [TestMethod]
+        public void TestAlternatingStrikesAndSpares()
+        {
+            _game.RollStrike();
+            _game.Roll(4, 6);
+            _game.RollStrike();
+            _game.Roll(7, 3);
+            _game.RollStrike();
+            _game.Roll(9, 1);
+            _game.RollStrike();
+            _game.Roll(5, 5);
+            _game.RollStrike();
+            _game.RollLastFrame(8, 2, 10);
+            Assert.AreEqual(200, _game.Score());
+        }
+
+        [TestMethod]
+        public void Test_UseCase()
+        {
+            _game.RollStrike();
+            _game.Roll(9, 1);
+            _game.Roll(5, 5);
+            _game.Roll(7, 2);
+            _game.RollStrike();
+            _game.RollStrike();
+            _game.RollStrike();
+            _game.Roll(9, 0);
+            _game.Roll(8, 2);
+            _game.RollLastFrame(9, 1,10);
+            Assert.AreEqual(187, _game.Score());
+        }
+
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        /* Key points I taken care of while doing development
+         1.Single Responsbility for game class.
+         2. Used Yield Keyword used for custom stateful itereation on list collection.
+         3.Created Extension method to add Logic for Better code mainatinablity.
+         4. Avoid looping.
+         5.Used comments so developer can understand.
+         6.Removed Extra Namespace
+         */
+
+        private readonly List<Frame> _frames = new List<Frame>();
+
+        public int Score()
+        {
+            Add(new Open(0, 0));
+            Add(new Open(0, 0));
+
+            for (int i = 0; i < 10; i++)
+                _frames[i].AddBonus(_frames[i + 1], _frames[i + 2]);
+
+            int counter = 0;
+            _frames.ForEach(frame => counter += frame.Score());
+            return counter;
+        }
+
+        public void Roll(int firstRoll, int secondRoll)
+        {
+            if (!GameFinished())
+            {
+                Add(Frame.Create(firstRoll, secondRoll));
+            }
+        }
+
+        public void RollStrike()
+        {
+            Roll(10, 0);
+        }
+
+        private bool GameFinished()
+        {
+            return _frames.Count.Equals(10);
+        }
+
+        private void Add(Frame frame)
+        {
+            _frames.Add(frame);
+        }
+
+        public void RollLastFrame(int firstRoll, int secondRoll, int thirdRoll)
+        {
+            Add(Frame.Create(firstRoll, secondRoll, thirdRoll));
+        }
+    }
+}
